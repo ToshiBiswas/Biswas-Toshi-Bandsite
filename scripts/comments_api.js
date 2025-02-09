98af0a56e8888f6f5c3cc095c6066b6ef434021c
ToshiBiswas <92240916+ToshiBiswas@users.noreply.github.com>
2025-02-09 13:17:28 -0700

    added api
    s

---
+++ scripts/comments_api.js
@@ -0,0 +1,41 @@
+
+class CommentApi{
+
+    static baseUrl = "https://unit-2-project-api-25c1595833b2.herokuapp.com/";
+    static id = null;
+    static async register(){
+        if (this.id == null){
+            const paragraph = await axios.get("https://unit-2-project-api-25c1595833b2.herokuapp.com/"+"register");
+            this.id = paragraph.data.api_key;
+        }
+
+    }
+    static async getComments(){
+        const response = await axios.get("https://unit-2-project-api-25c1595833b2.herokuapp.com/"+"comments/"+"?api_key="+String(CommentApi.id));
+
+        console.log(response);
+        response.data.sort((a, b) => {
+            return b.posted - a.posted;
+        });
+    
+        return response.data;
+    }
+    static async addComments(username, comment){
+        const newPost = {
+            name: username,
+            comment: comment
+        };
+        const response = await axios.post(
+            "https://unit-2-project-api-25c1595833b2.herokuapp.com/"+"comments/"+"?api_key="+String(CommentApi.id),
+            newPost
+        );
+        return response.data;
+
+    }
+}
+
+export default CommentApi;
+
+(async () => {
+    await CommentApi.register(); // Will trigger initialization if not already 
+})();
